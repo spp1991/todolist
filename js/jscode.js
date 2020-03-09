956a2adf5940912d63b0f8f69742fd1bad561bcb
Sunny Patel <spp1991@gmail.com>
2020-03-08 20:45:43 -0400

    Updated jscode.js file

--- js/jscode.js
+++ js/jscode.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
     updateArrays($(this).parent().parent().attr("taskno"));
     $(this).parent().remove();
     updateTaskCount();
-  })
+  });
 
   $("li").mouseover(function(){
     $(this).find("button").show();
